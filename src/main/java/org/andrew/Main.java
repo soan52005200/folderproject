6d563c3228214586ac6a03ab4d4d2390ef96d648
user <soan52005200@gmail.com>
2022-07-26 22:34:41 +0300

    end

--- src/main/java/org/andrew/Main.java
+++ src/main/java/org/andrew/Main.java
@@ -1,20 +1,44 @@
 package org.andrew;
 
+import org.apache.commons.io.FilenameUtils;
+
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) {
-        Path path = Paths.get(args[0]);
-        System.out.println("Hello world");
-        try (Stream<Path> entries = Files.walk(path)){
-            System.out.println();
-        }
-        catch (IOException e){
-            System.out.println(e);
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("C:\\new project\\src\\main\\resources");
+        setAllFilesPng(path);
+
+
+
+    }
+    public static void setAllFilesPng(Path path)
+
+            throws IOException {
+
+        try (Stream<Path> walk = Files.walk(path)) {
+            walk
+                    .filter(Files::isReadable)      // read permission
+                    .filter(Files::isRegularFile)   // file only
+                    .forEach(p -> {
+                        if (!FilenameUtils.getExtension(p.getFileName().toString()).equals("pdf")){
+
+                            try {
+                                Files.move(p, p.resolveSibling(p+".png"));
+                                //add file extention png
+
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
         }
     }
 }
